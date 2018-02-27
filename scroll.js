6250d7f55b05ccf47dcec11a9225d1de8dc2d619
AlbinH1 <te15.albin.olsson@umea.nti.se>
2018-02-27 13:56:19 +0100

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	$('a').click(function() {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 1500);
+		return false;
+	});
+});
